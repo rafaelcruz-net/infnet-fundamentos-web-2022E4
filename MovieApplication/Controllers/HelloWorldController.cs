bcd9831524d8bf708415d40603200baa20a5f9cf
rafael.cruz <rafael.cruz@SJ102-99>
2022-10-31 09:32:40 -0300

    Add project files.

---
+++ MovieApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApplication.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
